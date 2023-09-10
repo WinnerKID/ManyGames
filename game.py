ab2dfd5d4a9d29dee282b1de24ba2acfa1d419f9
richardp <richy.ronkerboy@gmail.com>
2023-09-09 20:40:11 -0400

    organized game if statements

--- game.py
+++ game.py
@@ -12,31 +12,32 @@ def guess_game():
 
     while True:
         try:
-            # Get the player's input
             player_input = input("Enter your guess: ")
-
-            # Check if the player wants to exit
-            if player_input.lower() == "exit":
-                main.exit_to_menu()
+            if player_input == "exit":
+                import main
+                main.menu_start()
                 break
-
             # Convert the player's input to an integer for guessing
             player_guess = int(player_input)
             attempts += 1
 
-            # Check if the guess is correct
-            if player_guess == secret_number:
-                print(f"\nCongratulations! You guessed the number {secret_number} in {attempts} attempts.\n")
-                import main
-                main.menu_start()
-                break
-            elif player_guess < secret_number:
-                print("Higher.")
-            else:
-                print("Lower.")
-
         except ValueError:
             print("Invalid input. Please enter a valid number or 'exit'.")
+            continue
+
+        # Check if the guess is correct
+        if player_guess == secret_number:
+            print(f"\nCongratulations! You guessed the number {secret_number} in {attempts} attempts.\n")
+            import main
+            main.menu_start()
+            break
+        elif player_guess < secret_number:
+            print("Higher.")
+        else:
+            print("Lower.")
+
+
+
 
 if __name__ == "__main__":
     guess_game()
